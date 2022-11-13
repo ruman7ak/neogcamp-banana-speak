682176452dd52f04b02c8d7407f63c89263700a0
Ruman Ak <ruman7ak@gmail.com>
2022-11-13 23:24:38 +0300

    feat:added error handler

--- app.js
+++ app.js
@@ -7,20 +7,22 @@ function getTranslationURL(text){
     return serverURL+"?"+"text="+text;
 }
 
+function errorHandler(error) {
+    console.log("error occured",error)
+    alert("Something wrong with the server! Try again after some time")
+}
+
 function clickHandler(){
     var inputTxt= txtArea.value;
 
+
      fetch(getTranslationURL(inputTxt))
-        .then(response => (response.json()))
-        .then (json =>{
-            var translatedText= (json.contents.translated);
-            outputDiv.innerText = translatedText;
-
-        });
-}
-
-
-
-
+        .then(response => response.json())
+        .then (json => {
+            var translatedTxt =  json.contents.translated;
+            outputDiv.innerText= translatedTxt
+        })
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener ("click", clickHandler)
