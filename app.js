6ac2a6f98168a3c68b6b6d2d6c8f10b00a36e6fe
Ruman Ak <ruman7ak@gmail.com>
2022-11-13 07:02:08 +0300

    intitate commit
    Added java to the html file and started taking the input commands

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate= document.querySelector("#btn-translate") ;
+var txtArea= document.querySelector("#txt-area");
+
+
+
+
+btnTranslate.addEventListener ("click", function addEventHandler(){
+    console.log("click");
+    console.log(txtArea.value);
+})
+
