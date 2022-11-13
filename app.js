e3f748c8d0cbe155ab4533f44a88415786678724
Ruman Ak <ruman7ak@gmail.com>
2022-11-13 22:27:34 +0300

    refactor: Added mock server and API

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var btnTranslate= document.querySelector("#btn-translate") ;
 var txtArea= document.querySelector("#txt-area");
 var outputDiv= document.querySelector("#output");
+var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text;
+}
+
+function clickHandler(){
+    var inputTxt= txtArea.value;
+
+     fetch(getTranslationURL(inputTxt))
+        .then(response => (response.json()))
+        .then (json =>{
+            var translatedText= (json.contents.translated);
+            outputDiv.innerText = translatedText;
+
+            });
+}
 
 
 
 
-btnTranslate.addEventListener ("click", function addEventHandler(){
-    outputDiv.innerText= ("djjdjd"+txtArea.value);
-})
 
+btnTranslate.addEventListener ("click", clickHandler)
